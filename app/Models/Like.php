b94b35e4b7341327ae4594fcc7877ed9a69bb0ae
三宅泰斗 <miyaketaito@miyaketaitonoMacBook-Pro.local>
2021-03-05 19:37:31 +0900

    relation構築

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Like extends Model
 {
-    //
+    //fillable指定してません
+
+    public function user(){
+        return $this->belongsTo("App\User");
+    }
+
+    public function music(){
+        return $this->belongsTo("App\Models\Music");
+    }
 }
