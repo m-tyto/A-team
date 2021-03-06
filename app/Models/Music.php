6cd86a88c9dca2cf307531bfb23d99337271d34c
山田徹 <yamadatooru@MacBook-Pro.local>
2021-03-06 23:20:45 +0900

    yamadaブランチ作成

--- app/Models/Music.php
+++ app/Models/Music.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Music extends Model
 {
-    //
+    protected $fillable = ["id", "artist", "title", "text", "link"];
+
+    public function category(){
+        return $this->belongsTo("App\Models\Category");
+    }
+
+    public function user(){
+        return $this->belongsTo("App\User");
+    }
+
+    public function likes(){
+        return $this->hasmany("App\Models\Like");
+    }
 }
