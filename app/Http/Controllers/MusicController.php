9975cc7328ba026737cf184cc86f8e32b5027caf
shatoo1551 <Einstein8tesla@gmail.com>
2021-03-18 19:01:16 +0900

    これは正しくない

--- app/Http/Controllers/MusicController.php
+++ app/Http/Controllers/MusicController.php
@@ -51,6 +51,7 @@ class MusicController extends Controller
             'user_id' => $user_id
             ]);;
     }
+    
     /**
      * Store a newly created resource in storage.
      *
@@ -162,39 +163,6 @@ class MusicController extends Controller
             $message = "検索結果ありません";
         }
     }
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 
     public function countlike(Request $request)
     {
@@ -209,4 +177,5 @@ class MusicController extends Controller
         ]);
         return back() ;
     }
+
 }
